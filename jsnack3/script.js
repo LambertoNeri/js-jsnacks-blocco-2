1fae50611dfafe3542dfa48122b3e4c62b5752e7
LambertoNeri <neri.lamberto91@gmail.com>
2023-04-18 19:06:50 +0200

    fix es. 3

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -2,17 +2,28 @@
 Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari  */
 
 const myArr = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-]
+   1,
+   2,
+   3,
+   4,
+   5,
+   6,
+   7,
+   8,
+   9,
+   10,
+   11,
+   12,
+];
 
-let addiction = myArr[0] + myArr[2] + myArr[4] + myArr[6] + myArr[8]
+let addiction = (0);
+
+/*let addiction = myArr[0] + myArr[2] + myArr[4] + myArr[6] + myArr[8]
+console.log (addiction) */
+
+for (let i = 0; i < myArr.length; i ++ ){
+    if (i % 2 !== 0) {
+        addiction += myArr[i];
+    }   
+}
 console.log (addiction)
