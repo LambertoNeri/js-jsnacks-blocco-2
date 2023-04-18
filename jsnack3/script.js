41a993c2c264a594735bff46ad5ad9426b255740
LambertoNeri <neri.lamberto91@gmail.com>
2023-04-18 16:23:03 +0200

    push esercizio 3

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,2 +1,18 @@
 /* snack 2.3
 Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari  */
+
+const myArr = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+]
+
+let addiction = myArr[0] + myArr[2] + myArr[4] + myArr[6] + myArr[8]
+console.log (addiction)
