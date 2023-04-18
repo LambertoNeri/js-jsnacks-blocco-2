a63f462bf2e258a80f303ad2018246abebf2da1c
LambertoNeri <neri.lamberto91@gmail.com>
2023-04-18 15:09:22 +0200

    NaN fix for snack1 + start snack2

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -3,28 +3,27 @@ Crea un array vuoto.
 Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array.  */
 let numbers;
-let verifier = false
+let verifier = false;
 let myArr = [];
 
 for ( let i = 0; i < 6; i++ ) {
-   numbers = parseInt(prompt("inserisci un numero"))
+   numbers = prompt("inserisci un numero")
    
    if (numbers % 2 == 0 ) {
     console.log ('pari', numbers);
+   } else if (isNaN(numbers)) {
+    console.log ('not a number', numbers)
    } else {
     verifier = true
     console.log ('dispari', numbers);
     if (verifier == true) {
-        parseInt(myArr.push(numbers))
+        (myArr.push(numbers))
     }
    }
 }
 
 
-for ( let i = 0; numbers.length; i++){
-    element => typeof element === 'number'
-    console.log ('dentro secondo', i)
-}
+
 
 console.log ('valore my Arr fuori',myArr)
 
